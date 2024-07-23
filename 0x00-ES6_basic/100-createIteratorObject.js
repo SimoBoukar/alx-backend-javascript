5dfd9757d40d0a6bbd99293f517749abb4ea3b48
Mohammed <6bouass9@gmail.com>
2024-07-23 04:44:55 +0100

    update advanced tasks

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   let employee = [];
-    for (let [depts, emplys] of Object.entries(report.allEmployees)){
-      for (let emp of emplys){
+    for (let [depts, emplys] of Object.entries(report.allEmployees)) {
+      for (let emp of emplys) {
         employee.push(emp);
       }
     }
